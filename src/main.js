284f8efc0a39e73f6438a4a622ec5e656f4f990b
Theareth-08 <thearethngeth123@gmail.com>
2025-01-31 15:32:38 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,53 @@
+// TOGGLE BUTTON
+const navMenu = document.getElementById("nav-menu")
+const navLink = document.querySelectorAll("nav-link")
+const hamburger = document.getElementById("hamburger")
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("left-[0]")
+  hamburger.classList.toggle('ri-close-large-line')
+})
+navLink.forEach(link=>{
+    link.addEventListener("click", () => {
+      navMenu.classList.remove("left-[0]")
+      hamburger.classList.remove('ri-close-large-line')
+    })
+})
+
+
+// SHOW SCROLL UP
+
+// CHANGE BACKGROUND HEADER
+
+// SWIPER
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
+
+// SCROLL SECTIONS ACTIVE LINK
+
+// SCROLL REVEAL ANIMATION
